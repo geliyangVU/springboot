2c6a55155d363a1ccdde487991bd9ae5b3961931
Geli Yang <gyang5@villanova.edu>
2022-04-27 22:05:30 -0400

    create dependency injection

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,10 +1,13 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudents() {
         return List.of(new Student(
