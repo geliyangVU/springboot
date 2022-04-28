0a9f4c00519a3a09d94ed6f43e40d83d7f3d8c2d
Geli Yang <gyang5@villanova.edu>
2022-04-27 21:50:00 -0400

    create Student class

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,7 +20,11 @@ public class DemoApplication {
 	@GetMapping
 	public List<Student> hello() {
 		return List.of(new Student(
-
+				1L,
+				"Mariam",
+				"mariam.jamal@gmail.com",
+				LocalDate.of(2000,1,1),
+				21
 				)
 		);
 	}
